85003556277723c7e58b716e2171aefbe30eba71
Mehedi Hasan Sarkar <mehedisarkar2k@gmail.com>
2023-03-11 14:57:32 +0600

    redis added for single post

--- app/controllers/posts/getPost.js
+++ app/controllers/posts/getPost.js
@@ -1,23 +1,37 @@
 // internal imports
-const Post = require("../../models/Post");
+const Post = require('../../models/Post');
+
+const redisClient = require('../../utils/redisClient');
 
 module.exports = async (req, res, next) => {
-    const { postId } = req.query;
+  const { postId } = req.query;
 
-    if (!postId) return next({ status: 404, message: "Invalid Post Id" });
+  const redisKey = `post:${postId}`;
 
-    try {
-        const post = await Post.findById(postId).populate(
-            "author",
-            "firstName lastName image"
-        );
-        // .populate("likes", "author image")
-        // .populate("comments", "author image createdAt");
+  if (!postId) return next({ status: 404, message: 'Invalid Post Id' });
 
-        if (!post) return next({ status: 404, message: "Invalid Post Id" });
+  try {
+    const cachedPost = await redisClient.get(redisKey);
 
-        res.send({ error: false, post });
-    } catch (error) {
-        next(error);
+    if (cachedPost) {
+      console.log('Serving from cache');
+      return res.send({ error: false, post: JSON.parse(cachedPost) });
     }
+
+    const post = await Post.findById(postId).populate(
+      'author',
+      'firstName lastName image'
+    );
+    // .populate("likes", "author image")
+    // .populate("comments", "author image createdAt");
+
+    if (!post) return next({ status: 404, message: 'Invalid Post Id' });
+
+    // set the posts in the redis cache
+    redisClient.set(redisKey, JSON.stringify(post));
+
+    res.send({ error: false, post });
+  } catch (error) {
+    next(error);
+  }
 };
